2901fb710afc8b3d5944241c714dd1cd95403212
corocn <crsk.plmzh@gmail.com>
2021-04-18 23:33:21 +0900

    get file names

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,29 @@
 #[macro_use] extern crate rocket;
 
 use rocket_contrib::templates::Template;
+use std::{env, fs};
+
+#[derive(serde::Serialize)]
+struct TemplateContext<'a> {
+    items: Vec<&'a str>,
+}
 
 #[get("/")]
 fn index() -> Template {
-    let context = {};
-    Template::render("index", &context)
+    let mut path_buf = env::current_dir().unwrap();
+    let dir = String::from("sample");
+    path_buf.push(dir);
+    let base_path = path_buf.to_str().unwrap();
+
+    // for entry in fs::read_dir(base_path) {
+    //     let path = entry.path();
+    //
+    //     dbg!(path.to_str().unwrap())
+    // }
+
+    Template::render("index", &TemplateContext {
+        items: vec!["One", "Two", "Three"],
+    })
 }
 
 fn main() {
