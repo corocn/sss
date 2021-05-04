2cbf59478f481c4ccf85bbe80323a19b92fedae8
corocn <crsk.plmzh@gmail.com>
2021-05-04 21:30:10 +0900

    sample

--- src/main.rs
+++ src/main.rs
@@ -4,136 +4,27 @@
 extern crate rocket;
 
 use handlebars::Handlebars;
-
-// #[macro_use]
-// extern crate serde_json;
-
-use rocket::config::{Config, Environment};
 use rocket::response::content;
-use rocket_contrib::serve::StaticFiles;
-// use rocket_contrib::templates::Template;
-use rocket::State;
-use serde::ser::SerializeStruct;
-use serde::{Serialize, Serializer};
-use std::path::{PathBuf, Path};
-use std::{fs, io};
-
-const TEMPLATE_STR: &str = include_str!("../templates/index.html.hbs");
 
 #[derive(serde::Serialize)]
 struct TemplateContext {
-    items: Vec<SoundFile>,
-}
-
-#[derive(Debug)]
-struct SoundFile {
     name: String,
-    path: String,
-    full_path: String,
-}
-
-struct MyConfig {
-    full_path: PathBuf
-}
-
-impl Serialize for SoundFile {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut s = serializer.serialize_struct("SoundFile", 3)?;
-        s.serialize_field("name", &self.name)?;
-        s.serialize_field("path", &self.path)?;
-        s.serialize_field("full_path", &self.full_path)?;
-        s.end()
-    }
-}
-
-fn get_files(base_path: &PathBuf) -> io::Result<Vec<PathBuf>> {
-    let mut files: Vec<PathBuf> = vec![];
-    for entry in fs::read_dir(base_path)? {
-        let entry = entry?;
-        let path = entry.path();
-        files.push(path);
-    }
-
-    Ok(files)
-}
-
-fn convert_sound_files(files: Vec<PathBuf>) -> Vec<SoundFile> {
-    let mut sound_files = vec![];
-    for path in files {
-        if let Some(x) = path_buf_to_sound_file(&path) {
-            sound_files.push(x);
-        }
-    }
-
-    sound_files
-}
-
-fn path_buf_to_sound_file(path: &PathBuf) -> Option<SoundFile> {
-    let name = path.file_name()?.to_str()?.to_string();
-    let full_path = path.to_str()?.to_string();
-    let path = format!("/_static/{}", name);
-
-    Some(SoundFile {
-        name,
-        full_path,
-        path,
-    })
 }
 
 #[get("/")]
-fn index(state: State<MyConfig>) -> content::Html<String> {
-    let files: Vec<PathBuf> = get_files(&state.full_path).unwrap();
-    let files: Vec<SoundFile> = convert_sound_files(files);
-
+fn index() -> content::Html<String> {
     let reg = Handlebars::new();
     let rendered = reg
-        .render_template(TEMPLATE_STR, &TemplateContext { items: files })
+        .render_template(
+            include_str!("../templates/index.html.hbs"),
+            &TemplateContext {
+                name: String::from("Yamada Taro"),
+            },
+        )
         .unwrap();
     content::Html(rendered)
 }
 
-// TODO: ファイルの制限
-// TODO: 絞り込み
-
-use structopt::StructOpt;
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "sss", author = "Takahiro Tsuchiya @corocn")]
-struct Opt {
-    #[structopt(short, long, default_value = "localhost")]
-    bind_address: String,
-
-    #[structopt(short, long, default_value = "8000")]
-    port: u16,
-
-    #[structopt(name = "TARGET_DIR", default_value = ".")]
-    dir: String,
-}
-
-fn main() -> std::io::Result<()> {
-    let opt = Opt::from_args();
-
-    let full_path = fs::canonicalize(Path::new(&opt.dir))?;
-    let config = MyConfig {
-        full_path: full_path.to_owned()
-    };
-
-    println!("{:?} is mapped.", &config.full_path);
-
-    let rocket_config = Config::build(Environment::Production)
-        .address(opt.bind_address)
-        .port(opt.port)
-        .finalize()
-        .unwrap();
-
-    rocket::custom(rocket_config)
-        .mount("/", routes![index])
-        .mount("/_static", StaticFiles::from(&config.full_path))
-        .manage(config)
-        .launch();
-
-    Ok(())
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
